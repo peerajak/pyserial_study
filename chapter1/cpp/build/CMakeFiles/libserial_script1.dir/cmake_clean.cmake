b501132b6325c9f5ae581534031a20ef6c32920d
peerajak <peerajak@gmail.com>
2025-07-09 09:09:57 +0700

    first commit

---
+++ chapter1/cpp/build/CMakeFiles/libserial_script1.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/libserial_script1.dir/src/libserial_script1.cpp.o"
+  "CMakeFiles/libserial_script1.dir/src/libserial_script1.cpp.o.d"
+  "libserial_script1"
+  "libserial_script1.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/libserial_script1.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ chapter1/cpp/build/CMakeFiles/libserial_script1.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for libserial_script1.
+# This may be replaced when dependencies are built.
